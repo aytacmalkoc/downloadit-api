9d62a71a766d6629f85ddba4b994c9c665742de0
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-09-09 08:23:00 +0300

    Removed utils

--- utils/index.js
+++
@@ -1,9 +0,0 @@
-const downloadFromYouTube = require('./youtube');
-const downloadFromTwitter = require('./twitter');
-const downloadFromReddit = require('./reddit');
-
-module.exports = {
-    downloadFromYouTube,
-    downloadFromTwitter,
-    downloadFromReddit
-}
