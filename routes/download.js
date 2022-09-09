0bff0e64271738dce0fb13cfb9ac8958179eb9f0
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-09-09 08:22:46 +0300

    Code readability is improved

--- routes/download.js
+++ routes/download.js
@@ -1,34 +1,14 @@
 const express = require('express');
-const { downloadFromYouTube, downloadFromTwitter, downloadFromReddit} = require('../utils');
+const downloader = require('../helpers/downloader');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
     const { type, url } = req.query;
-    let data;
 
-    if (type === 'youtube') {
-      data = await downloadFromYouTube(url);
-    }else if (type === 'twitter') {
-      data = await downloadFromTwitter(url);
-    }else if (type === 'reddit') {
-      data = await downloadFromReddit(url);
-    }else {
-      return res.json({
-        message: 'invalid type',
-        available_types: ['youtube', 'twitter', 'reddit']
-      }).status(400);
-    }
-
-    return res.json({
-      data
-    }).status(200);
+    return res.json(await downloader(type, url)).status(200);
   } catch (error) {
-    return res.json({
-      message: error.message,
-      error
-    }).status(400);
+    return res.json(error);
   }
 });
 
